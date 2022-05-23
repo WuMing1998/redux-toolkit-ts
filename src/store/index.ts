55b45d91921482cbc90dce994ab9ca5193f782b2
chengkun <3261937664@qq.com>
2022-05-23 18:21:20 +0800

    add configureStore() api

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,33 +1,3 @@
-hello,这里是学习 redux-toolkit 的笔记和实验项目。
-
-## 安装/install
-
-```cmd
-    npx create-react-app my-app --template redux-typescript
-```
-
-```yarn
-    yarn add @reduxjs/toolkit
-```
-
-## Api:
-
-### configureStore()
-
-利用简化的配置选项和默认设置来设置你的 createStore,可以扩展各种中间件。默认内置 redux-thunk，默认开启 Redux DevTools Extension。
-
-```ts
-// file: /store/todos/todosReducer.ts noEmit
-import { Reducer } from "@reduxjs/toolkit";
-declare const reducer: Reducer<{}>;
-export default reducer;
-
-// file: /store/visibility/visibilityReducer.ts noEmit
-import { Reducer } from "@reduxjs/toolkit";
-declare const reducer: Reducer<{}>;
-export default reducer;
-
-// file: /store/index.ts
 import { configureStore } from "@reduxjs/toolkit";
 
 //中间件插件示例：添加redux-logger中间件
@@ -75,4 +45,3 @@ const store = configureStore({
 // 内置redux-thunk和redux-logger中间件
 // 自动检测开发环境与生产环境开启devTools
 // 包含redux扩展器 使用redux-batch扩展redux
-```
