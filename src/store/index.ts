20df1acdd1bb78c45789100397fc76a0a0153d86
chengkun <3261937664@qq.com>
2022-06-03 17:58:44 +0800

    update store/index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,16 +5,13 @@ import logger from "redux-logger";
 
 //添加redux扩展功能 redux-batch 批处理
 import { reduxBatch } from "@manaflair/redux-batch";
-
 import todosReducer from "./todos/todosReducer";
 
-const reducer = {
-  todos: todosReducer,
-};
-
-export const store = configureStore({
+export default configureStore({
   //reducer
-  reducer,
+  reducer: {
+    todos: todosReducer,
+  },
   //使用redux-logger中间件
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
   //生产环境关闭devTools // 安装dev工具  chrome应用商店 安装 React Developer Tools
