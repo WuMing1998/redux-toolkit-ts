5b07ccd15952f1eb949f54be00da79ec4f6e6293
wuming <3261937664@qq.com>
2022-06-07 20:08:52 +0800

    案例

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { FC } from "react";
 import { addTodo } from "./store/todos/todosReducer";
+import { Counter } from "./features/counter/Counter";
 
 const App: FC = () => {
   return (
     <div className="App">
+      <Counter></Counter>
     </div>
   );
 }
